78ce47eaf8e30124349195c33ef2574fad7ff5cf
Vinicius Figueiredo Nogueira <comp.vinicius@gmail.com>
2023-06-01 18:36:37 -0300

    Initial commit

---
+++ src/main/java/com/alianza/exercise/opentrivia/OpenTriviaExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.alianza.exercise.opentrivia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenTriviaExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenTriviaExerciseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alianza/exercise/opentrivia/OpenTriviaExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alianza.exercise.opentrivia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenTriviaExerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
